0de7392ec4758d51e38f85d76fca8e48a4048d6b
Eudris Cabrera <ecabrera@hacienda.gov.do>
2020-11-20 16:36:16 -0400

    text translation

--- src/jbake/assets/js/JSON-translation-data.js
+++ src/jbake/assets/js/JSON-translation-data.js
@@ -116,13 +116,13 @@ var linguJSON = {
     },
 
     {
-      "source": "Community",
-      "target_es": "Comunidad",
+      "source": "<strong>Community</strong>",
+      "target_es": "<strong>Comunidad</strong>",
     },
 
     {
-      "source": "Interests",
-      "target_es": "Intereses",
+      "source": "<strong>Interests</strong>",
+      "target_es": "<strong>Intereses</strong>",
     },
     {
       "source": "<strong>Professional Experience</strong>",
@@ -185,8 +185,8 @@ var linguJSON = {
       "target_es": "Música, Baile Latino"
     },
     {
-      "source":"<strong>Sport :</strong> Baseball, Softball, Strength Training",
-      "target_es": "<strong>Deporte :</strong> Béisbol, Softbol, Entrenamiento de Resistencia"
+      "source":"<strong>Sport :</strong> Baseball, Softball, Strength Training, Jump Rope",
+      "target_es": "<strong>Deporte :</strong> Béisbol, Softbol, Entrenamiento de Resistencia, Jump Rope"
     },
     {
       "source":"Read more",
@@ -200,6 +200,204 @@ var linguJSON = {
     {
       "source":"Next post",
       "target_es":"Siguiente"
+    },
+    {
+      "source":"More Resources",
+      "target_es":"Información Adicional"
+    },
+    {
+      "source":"Presentation Slides",
+      "target_es":"Presentación"
+    },
+
+    {
+      "source":"Demo project",
+      "target_es":"Proyecto de demostración"
+    },
+
+    {
+      "source":"View my verified achievements from Acclaim Badging on",
+      "target_es":"Verificar mis cursos en"
+    },
+
+    {
+      "source":"<strong>Spanish : Native Language</strong>",
+      "target_es":"<strong>Español : Nativo</strong>"
+    },
+
+    {
+      "source":"<strong>English : Fluent (reading); Intermediate (speaking, writing)</strong>",
+      "target_es":"<strong>Inglés: Fluido (lectura); Intermedio (hablar, escribir)</strong>"
+    },
+
+    {
+      "source":"January",
+      "target_es":"Enero"
+    },
+
+    {
+      "source":"March",
+      "target_es":"Marzo"
+    },
+
+    {
+      "source":"May",
+      "target_es":"Mayo"
+    },
+    {
+      "source":"July",
+      "target_es":"Julio"
+    },
+
+    {
+      "source":"August",
+      "target_es":"Agosto"
+    },
+    {
+      "source":"September",
+      "target_es":"Septiembre"
+    },
+    {
+      "source":"<strong>Technical Writing</strong>",
+      "target_es":"<strong>Escritura Técnica</strong>"
+    },
+
+    {
+      "source":"<strong>Conferences & Presentations</strong>",
+      "target_es":"<strong>Conferencias Y Presentaciones</strong>"
+    },
+
+    {
+      "source":"<strong>Telematics Engineer, Computer Systems Networking and Telecommunications</strong>",
+      "target_es":"<strong>Ingeniero Telemático</strong>"
+    },
+
+    {
+      "source":"<strong>Containers</strong>",
+      "target_es":"<strong>Contenedores</strong>"
+    },
+
+    {
+      "source":"<strong>Languages and Specifications</strong>",
+      "target_es":"<strong>Lenguajes y Especificaciones</strong>"
+    },
+
+    {
+      "source":"<strong>Software Engineering</strong>",
+      "target_es":"<strong>Ingeniería de Software</strong>"
+    },
+
+    {
+      "source":"<strong>Application Servers</strong>",
+      "target_es":"<strong>Servidores de Aplicaciones</strong>"
+    },
+
+    {
+      "source":"<strong>Frameworks and Libraries</strong>",
+      "target_es":"<strong>Frameworks y Librerías</strong>"
+    },
+
+    {
+      "source":"<strong>Operative Systems</strong>",
+      "target_es":"<strong>Sistemas Operativos</strong>"
+    },
+
+    {
+      "source":"Unit Testing, Integration Testing",
+      "target_es":"Pruebas Unitarias, Pruebas de Integración"
+    },
+
+    {
+      "source":"<strong>Tools</strong>",
+      "target_es":"<strong>Herramientas</strong>"
+    },
+
+    {
+      "source":"Aug 2001 → Aug 2006",
+      "target_es":"Agosto de 2001 → Agosto de 2006"
+    },
+
+    {
+      "source":"March → May 2013",
+      "target_es":"Marzo → Mayo 2013"
+    },
+
+    {
+      "source":"May 30 → July 13, 2014",
+      "target_es":"30 de Mayo → 13 de Julio de 2014"
+    },
+
+    {
+      "source":"August → September 2014",
+      "target_es":"Agosto → Septiembre 2014"
+    },
+
+    {
+      "source":"July 6 → July 9, 2015",
+      "target_es":"6 de Julio → 9 de Julio de 2015"
+    },
+
+    {
+      "source":"July 14 → August 11, 2015",
+      "target_es":"14 de Julio → 11 de Agosto de 2015"
+    },
+
+    {
+      "source":"August 4 → 22 September, 2015",
+      "target_es":"4 de Agosto → 22 de Septiembre de 2015"
+    },
+
+    {
+      "source":"May 2017",
+      "target_es":"Mayo de 2017"
+    },
+    {
+      "source":"September 2018",
+      "target_es":"Septiembre de 2018"
+    },
+
+    {
+      "source":"January 2020",
+      "target_es":"Enero de 2020"
+    },
+
+    {
+      "source":"May 2020",
+      "target_es":"Mayo 2020"
+    },
+    {
+      "source":"<strong>Jan 2017 → Current</strong>",
+      "target_es":"<strong>Enero 2017 → Actual</strong>"
+    },
+
+    {
+      "source":"<strong>Jan 2013 → Dec 2016</strong>",
+      "target_es":"<strong>Enero de 2013 → Diciembre de 2016</strong>"
+    },
+
+    {
+      "source":"<strong>Mar 2012 → Dec 2012</strong>",
+      "target_es":"<strong>Marzo de 2012 → Diciembre de 2012</strong>"
+    },
+
+    {
+      "source":"<strong>Jul 2010 → Mar 2012</strong>",
+      "target_es":"<strong>Julio de 2010 → Marzo de 2012</strong>"
+    },
+
+    {
+      "source":"<strong>Jul 2010 → Mar 2012</strong>",
+      "target_es":"<strong>Julio de 2010 → Marzo de 2012</strong>"
+    },
+
+    {
+      "source":"<strong>Jul 2007 → Jun 2010</strong>",
+      "target_es":"<strong>Julio de 2007 → Junio de 2010</strong>"
+    },
+
+    {
+      "source":"<strong>Feb 2007 → Jul 2007</strong>",
+      "target_es":"<strong>Febrero de 2007 → Julio de 2007</strong>"
     }
 
   ]
